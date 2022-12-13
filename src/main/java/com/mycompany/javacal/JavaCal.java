0b56eb509e22e1219cfb1fe02063aa72bc9d321f
Windula Dissanayake <charithwinwindula@gmail.com>
2022-12-13 19:21:06 +0530

    commit files

---
+++ src/main/java/com/mycompany/javacal/JavaCal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.javacal;
+
+/**
+ *
+ * @author chari
+ */
+public class JavaCal {
+
+    public static void main(String[] args) {
+        JavaCalFrame cal = new JavaCalFrame();
+        cal.setVisible(true);
+    }
+}
